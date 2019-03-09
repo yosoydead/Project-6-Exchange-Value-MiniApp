34d176813ed4cf4e23f83fdc9bd9e141cb0acdfa
yosoydead <radu-bogdan@outlook.com>
2019-03-09 16:45:59 +0200

    getting started with parsing xml. only fetched and loaded basic elements

---
+++ curs valutar/ApiCall/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace curs_valutar.ApiCall
+{
+    public class Parser
+    {
+        //this is the link for the api xml call
+        private static string xmlUrl = @"http://www.bnr.ro/nbrfxrates.xml";
+
+        //this grabs the entire xml document
+        private static XDocument doc = XDocument.Load(xmlUrl);
+
+        //if i dont access the root element of the document, i cant iterate over the document
+        //it just returns me the whole document and it cant be worked with
+        //i also use the IEnumerable interface to make the root element a collection of elements from the xml document
+        //in total i would have only two elements
+        private static IEnumerable<XElement> root = doc.Root.Elements();
+        
+    }
+}
