dd70bfa831d5848044f935f6b6f95a9c9cb8cbf7
yosoydead <radu-bogdan@outlook.com>
2019-03-10 20:54:53 +0200

    validating input to accept numbers. error sets bgcolor to red as indicator.

--- curs valutar/Views/Body.xaml.cs
+++ curs valutar/Views/Body.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace curs_valutar.Views
 {
@@ -23,6 +24,29 @@ namespace curs_valutar.Views
         public Body()
         {
             InitializeComponent();
+        }
+
+        //a function that should validate the input field to be only numbers
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.]+");
+            //e.Handled = regex.IsMatch(e.Text);
+
+            //if the input is not valid, set the background color of the textbox to a red color
+            //to let the user know that the input is not valid and it will not be added to the box
+            if (regex.IsMatch(e.Text))
+            {
+                var red = Color.FromRgb(237, 28, 36);
+                SolidColorBrush brush = new SolidColorBrush(red);
+                NumberTextBox.Background = brush;
+                e.Handled = true;
+            }
+            else
+            {
+                //if the input is valid, the background should stay white
+                NumberTextBox.Background = Brushes.White;
+                e.Handled = false;
+            }
         }
     }
 }
